5503e4701546b2713316de8e90f1130ff6a41dcd
Michael Fairley <michaelfairley@gmail.com>
2014-01-03 16:49:34 -0600

    Bcrypt passwords

--- api/user.go
+++ api/user.go
@@ -1,8 +1,25 @@
 package api
 
+import (
+	"github.com/michaelfairley/mapi-tigertonic-gorp/code.google.com/p/go.crypto/bcrypt"
+	"github.com/michaelfairley/mapi-tigertonic-gorp/github.com/coopernurse/gorp"
+)
+
 type User struct {
-	Id       uint64 `json:"-"`
-	Username string `json:"username"`
-	Password string `json:"password,omitempty"`
-	Realname string `json:"real_name"`
+	Id              uint64 `json:"-"`
+	Username        string `json:"username"`
+	Password        string `json:"password,omitempty" db:"-"`
+	Realname        string `json:"real_name"`
+	CryptedPassword []byte `json:"-" db:"password"`
+}
+
+func (u *User) PreInsert(s gorp.SqlExecutor) error {
+	cryptedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.CryptedPassword = cryptedPassword
+
+	return nil
 }
